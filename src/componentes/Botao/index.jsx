ed56fcdffb0ab1e8fb7166ef6fce9cadbdb02a84
Pedro Bruno <pedrobn97@outlook.com>
2026-08-27 17:51:19 -0300

    feat: add styles for FormularioEvento

---
+++ src/componentes/Botao/index.jsx
@@ -0,0 +1,5 @@
+import './botao.estilos.css';
+
+export function BotaoFormulario ({children}) {
+    return <button className="botao-formulario">{children}</button>;
+}
--- src/componentes/FormularioEvento/index.jsx
+++ src/componentes/FormularioEvento/index.jsx
@@ -1,23 +1,39 @@
-import './formulario-de-eventos.estilos.css';
+import "./formulario-de-eventos.estilos.css";
 
 import { FormularioCampo } from "../CampoDeFormulario";
 import { FormularioTitulo } from "../TituloFormulario";
 import { Label } from "../Label";
 import { CampoEntrada } from "../CampoDeEntrada";
+import { BotaoFormulario } from "../Botao";
 
 export function FormularioDeEventos() {
   return (
     <form className="formulario-evento">
       <FormularioTitulo>Titulo do Form</FormularioTitulo>
-      <FormularioCampo>
-        <Label htmlFor="nomeEvento">Nome do Evento:</Label>
-        <CampoEntrada 
-          type="text" 
-          id="nome" 
-          placeholder="summer dev hits"
-          name="nomeEvento"  
-        />
-      </FormularioCampo>
+      <div className="campos-formulario">
+        <FormularioCampo>
+          <Label htmlFor="nomeEvento">Nome do Evento:</Label>
+          <CampoEntrada
+            type="text"
+            id="nomeEvento"
+            placeholder="summer dev hits"
+            name="nomeEvento"
+          />
+        </FormularioCampo>
+
+        <FormularioCampo>
+          <Label htmlFor="dataEvento">Data do Evento:</Label>
+          <CampoEntrada
+            type="date"
+            id="dataEvento"
+            placeholder="summer dev hits"
+            name="dataEvento"
+          />
+        </FormularioCampo>
+      </div>
+      <div>
+        <BotaoFormulario>Criar Evento</BotaoFormulario>
+      </div>    
     </form>
   );
 }
