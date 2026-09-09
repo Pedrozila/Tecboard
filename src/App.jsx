e5bcbe5f3407df10d669e72c34a085553bbaded7
Pedro Bruno <pedrobn97@outlook.com>
2026-09-09 17:50:23 -0300

    feat: new tema sections component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import "./App.css";
 import { FormularioDeEventos } from "./componentes/FormularioEvento";
+import { Tema } from "./componentes/Tema";
 
 function App() {
+
+  const temas = [
+    {
+      id: "1",
+      nome: 'Front-End'
+    },
+    {
+      id: "2",
+      nome: 'Front-End'
+    },
+    {
+      id: "3",
+      nome: 'Front-End'
+    },
+    {
+      id: "4",
+      nome: 'Front-End'
+    },
+    {
+      id: "5",
+      nome: 'Front-End'
+    },
+    {
+      id: "6",
+      nome: 'Front-End'
+    },
+  ]
+
   return (
     <main>
       <header>
@@ -11,6 +40,24 @@ function App() {
         <img src="/banner.png" alt="" />
       </section>
       <FormularioDeEventos />
+      <section>
+         <Tema tema={temas[0]} /> 
+      </section>
+      <section>
+         <Tema tema={temas[1]} /> 
+      </section>
+      <section>
+         <Tema tema={temas[2]} /> 
+      </section>
+      <section>
+         <Tema tema={temas[3]} /> 
+      </section>
+      <section>
+         <Tema tema={temas[4]} /> 
+      </section>
+      <section>
+         <Tema tema={temas[5]} /> 
+      </section>
     </main>
   );
 }
