8a44b8c3bbd940621e3160d21481574156ce287a
Pedro Bruno <pedrobn97@outlook.com>
2026-09-21 16:16:26 -0300

    feat: added map function to temas array

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,34 @@ import { FormularioDeEventos } from "./componentes/FormularioEvento";
 import { Tema } from "./componentes/Tema";
 
 function App() {
-
   console.log("App renderizou");
 
   const temas = [
     {
       id: "1",
-      nome: 'Front-End'
+      nome: "Front-End",
     },
     {
       id: "2",
-      nome: 'Front-End'
+      nome: "Front-End",
     },
     {
       id: "3",
-      nome: 'Front-End'
+      nome: "Front-End",
     },
     {
       id: "4",
-      nome: 'Front-End'
+      nome: "Front-End",
     },
     {
       id: "5",
-      nome: 'Front-End'
+      nome: "Front-End",
     },
     {
       id: "6",
-      nome: 'Front-End'
+      nome: "Front-End",
     },
-  ]
+  ];
 
   return (
     <main>
@@ -41,26 +40,14 @@ function App() {
       </header>
       <Banner />
       <FormularioDeEventos />
-      <section>
-         <Tema tema={temas[0]} /> 
-      </section>
-      <section>
-         <Tema tema={temas[1]} /> 
-      </section>
-      <section>
-         <Tema tema={temas[2]} /> 
-      </section>
-      <section>
-         <Tema tema={temas[3]} /> 
-      </section>
-      <section>
-         <Tema tema={temas[4]} /> 
-      </section>
-      <section>
-         <Tema tema={temas[5]} /> 
-      </section>
+      {temas.map(function (item) {
+        return (
+          <section>
+            <Tema tema={item} />
+          </section>
+        );
+      })}
     </main>
-
   );
 }
 
